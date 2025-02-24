762ef6b4d2a18ca0d6911dc238fca523132a404a
Illia0807 <illia.andriienko777@gmail.com>
2025-02-24 22:17:05 +0200

    d ch

---
+++ Week4/Day2/DailyChallenge/ch2.js
@@ -0,0 +1,25 @@
+// let star="*"
+// let count=1
+// do{
+//         console.log(`${star}`)
+//         star+="*"
+//         count++
+
+// }while(count<7)
+
+// let star=""
+// for(i=0;i<6;i++){
+//         for(j=2;j<i;j++){
+//                 star+="*"
+//                 console.log(star)
+//         }
+// }
+
+// for (let i = 0; i < 6; i++) {
+//         let star = '';
+//         for (let j = 0; j <= i; j++) {
+//           star = star + '*';
+//         }
+//         console.log(star);
+//       }
+
